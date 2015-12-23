4372408cae3b0c1b4cc5ef461864d5e7ae8d79d6
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-23 13:35:31 -0500

    abstract into generate URI function

--- src/get-uniprot.js
+++ src/get-uniprot.js
@@ -31,63 +31,27 @@ const xmlParseAsync = function (str) {
  */
 function gUri(base, id, suffix) {
   // TODO handle base/ vs. base
-  // const str = base
-  // if (base[base.length()-1] === '/') {
-  //   base +=
-  // }
 
   return base + id + suffix
 }
 
 
-const UniProtKBBaseUri = 'http://www.uniprot.org/uniprot/'
-const suffix = '.xml'
-
 let filtered = {}
 
 // take Object stream of parsed ndjson of QuickGO annotations
 process.stdin
-  // .pipe(through.obj(function (obj, enc, cb) {
-  //   // get UniProtKB XML for each protein
-  //   const UniProtKBBaseUri = 'http://www.uniprot.org/uniprot/'
-  //   const suffix = '.xml'
-  //   const uri = gUri(UniProtKBBaseUri, obj.ID, suffix)
-  //   console.log(uri)
-  //
-  //
-  //
-  //
-  //   this.push(uri)
-  //
-  //   cb()
-  // }))
-  // .pipe(ndjson.parse())
-  // .pipe(through.obj(function (obj, enc, cb) {
-  //   this.push(JSON.stringify(obj) + '\n')
-  //   cb()
-  // }))
-  // .pipe(through.obj(function (chunk, enc, cb) {
-  //   this.push(typeof(chunk))
-  //   console.log(chunk)
-  //   // cb()
-  // }))
-  // .pipe(process.stdout)
   .pipe(ndjson.parse())
   .on('data', function (obj) {
     if (obj.DB === 'UniProtKB') {
       co.wrap(function* () {
         try {
-          let xml = yield requestAsync(UniProtKBBaseUri + obj.ID + '.xml')
-            // console.log(xml)
+          const UniProtKBBaseUri = 'http://www.uniprot.org/uniprot/'
+          const suffix = '.xml'
+          let xml = yield requestAsync(gUri(UniProtKBBaseUri, obj.ID, '.xml'))
           let xmlJSON = yield xmlParseAsync(xml)
-            // console.log(xmlJSON)
-            // console.log(xmlJSON.uniprot.entry[0])
 
-          // console.log(xmlJSON.uniprot.entry.gene)
           if (xmlJSON.uniprot.entry[0].gene) {
-            // console.log(xmlJSON.uniprot.entry[0].gene[0].name[0]._)
             const geneName = xmlJSON.uniprot.entry[0].gene[0].name[0]._
-            // console.log(geneName)
 
             if (Object.keys(filtered).indexOf(geneName) === -1) {
               filtered[geneName] = [xmlJSON.uniprot.entry[0]]
@@ -97,15 +61,6 @@ process.stdin
           }
 
           console.log(filtered)
-
-          // console.log('data', JSON.stringify(xmlJSON) + '\n')
-
-          // console.log(xmlJSON.entry)
-          // if (xmlJSON.entry.uniprot.gene !== 'undefined') {
-          //   console.log(xmlJSON.entry.uniprot.gene)
-          // }
-
-          // console.log(xmlJSON.uniprot.entry.gene[0].name[0]._)
         } catch (e) {
           console.error(e)
         }
